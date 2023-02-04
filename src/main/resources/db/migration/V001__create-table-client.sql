94840a15531e2102db9b9108140f54d1eaefdd78
rian33svan <96176662+RianErick@users.noreply.github.com>
2023-02-04 14:51:08 -0300

    New Tables Add

--- src/main/resources/db/migration/V001__create-table-client.sql
+++ src/main/resources/db/migration/V001__create-table-client.sql
@@ -1,6 +1,24 @@
 create table cliente(
-id bigint not null auto_increment,
-nome varchar (60) not null,
-email varchar (255) not null,
-primary key (id)
+id integer primary key auto_increment,
+nome varchar (60) not null
+);
+
+create table produto(
+id integer primary key auto_increment,
+descricao varchar (100) not null,
+preco_unitario numeric (20,2)
+);
+
+create table pedido(
+id integer primary key auto_increment,
+cliente_id integer references cliente (id),
+data_pedido timestamp ,
+total numeric(20,2)
+);
+
+create table item_pedido(
+id integer primary key auto_increment,
+pedido_id integer references pedido(id),
+produto_id integer references produto(id),
+quantidade integer
 );
